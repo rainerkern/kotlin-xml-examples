a472c66505ac95367d2c7ed0437d3851412bc2da
Rainer Kern <11457729+rainerkern@users.noreply.github.com>
2019-03-15 17:11:10 +0100

    Simplify constructor

--- src/main/kotlin/org/fabiomsr/kotlinxml/_02_nested_object/model/Name.kt
+++ src/main/kotlin/org/fabiomsr/kotlinxml/_02_nested_object/model/Name.kt
@@ -5,8 +5,7 @@ import org.simpleframework.xml.Element
 /**
  * Created by fabiomsr on 3/7/16.
  */
-data class Name(@field:Element var firstname: String,
-                @field:Element var lastname: String) {
-
-    constructor( ) : this("", "") {}
-}
+data class Name(
+    @field:Element var firstname: String = "",
+    @field:Element var lastname: String = ""
+)
